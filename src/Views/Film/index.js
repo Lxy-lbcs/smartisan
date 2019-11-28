e65938eb709b5583318a637cbcff2ff11f1ad5d6
Lxy-lbcs <lxy15235491996@outlook.com>
2019-11-28 09:58:29 +0800

    11

--- src/Views/Film/index.js
+++ src/Views/Film/index.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import style from './style.module.css'
 
 export default class index extends Component {
   render() {
     return (
-      <div>
-        Film
+      <div className={style.all}>
+        <header className={style.header}>
+          <figure className={style.logo}></figure>
+        </header>
+        <section className={style.section}>
+          <div className={style.search}>
+            <aside className={style.search_icon}></aside>
+            <aside className={style.search_val}>请输入搜索关键字</aside>
+          </div>
+        </section>
+        <main className={style.main}>
+
+        </main>
+        <aside className={style.aside}>
+
+        </aside>
       </div>
     )
   }
