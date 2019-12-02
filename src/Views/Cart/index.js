8c2f7bbfad60f1e277918c95bf06262e996dfd68
Lxy-lbcs <lxy15235491996@outlook.com>
2019-12-02 08:48:43 +0800

    Lxy购物车修改

--- src/Views/Cart/index.js
+++ src/Views/Cart/index.js
@@ -8,6 +8,7 @@ class index extends Component {
   state = {
     haveCommodity: true,
     cartList: null,
+    recommendList: null,
     isshow: true
   }
 
@@ -84,6 +85,103 @@ class index extends Component {
                     }
                   </ul>
                 </div>
+                <div className={style.recommend}>
+                  <div className={style.recommend_title}>
+                    <h2>相关推荐</h2>
+                  </div>
+                  <div className={style.recommend_container}>
+                    <ul style={{
+                      overflow: 'hidden',
+                      padding: '.9rem .6rem 0',
+                      marginBottom: '80px'
+                    }}>
+                      {
+                        this.state.recommendList === null ? null :
+                          this.state.recommendList.map(item => (
+                            <li key={item.id} style={{
+                              width: '50%',
+                              float: 'left',
+                            }} >
+                              <div>
+                                <a href="/film" style={{
+                                  display: 'block',
+                                  position: 'relative',
+                                  paddingTop: '100%',
+                                  borderRadius: '.2rem'
+                                }}>
+                                  <img src={item.shop_info.ali_image} alt="" style={{
+                                    position: 'absolute',
+                                    left: 0,
+                                    top: 0,
+                                    width: '100%',
+                                    height: '100%',
+                                    borderRadius: '.2rem'
+                                  }} />
+                                </a>
+                                <div style={{
+                                  margin: '0 auto',
+                                  padding: '.4rem .6rem .9rem'
+                                }}>
+                                  <h4 style={{
+                                    overflow: 'hidden',
+                                    textOverflow: 'ellipsis',
+                                    whiteSpace: 'nowrap',
+                                    margin: 0,
+                                    fontSize: '.6rem',
+                                    color: '#333'
+                                  }}>
+                                    {item.name}
+                                  </h4>
+                                  <p style={{
+                                    marginTop: '.3rem',
+                                    overflow: 'hidden',
+                                    textOverflow: 'ellipsis',
+                                    whiteSpace: 'nowrap',
+                                    fontSize: '.5rem',
+                                    lineHeight: '1.2',
+                                    color: '#7f7f7f',
+                                    minHeight: '.6rem'
+                                  }}>
+                                    {item.shop_info.sku_mobile_sub_title}
+                                  </p>
+                                  <span style={{
+                                    color: '#d44d44',
+                                    display: 'inline-block',
+                                    whiteSpace: 'nowrap',
+                                    fontWeight: '700',
+                                    position: 'relative',
+                                    top: '.15rem',
+                                    left: '-.1rem',
+                                    fontSize: '.6rem'
+                                  }}>
+                                    ￥{item.spu.price}.00
+                                  </span>
+                                </div>
+                              </div>
+                            </li>
+                          ))
+                      }
+                    </ul>
+                  </div>
+                </div>
+                <div style={{
+                  position: 'fixed',
+                  height: '54px',
+                  padding: ' 0 12px',
+                  bottom: '56px',
+                  left: 0,
+                  right: 0,
+                  zIndex: '90',
+                  background: '#fafafa',
+                  boxShadow: '0 -0.2rem 1rem rgba(0,0,0,.1)',
+                  fontSize: '12px',
+                  color: '#666',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  boxSizing: 'border-box',
+                 
+                }}></div>
               </div>
           }
         </main>
@@ -114,7 +212,12 @@ class index extends Component {
     })
 
     Axios({
-      url: ''
+      url: '/product/skus?ids=100047001,100061001,100060501,100060201,100060201,100060202,100061001,100060901,100060401,100060601,100061101,100061201,100062701,100063401,100061801,100059001,100052801,100023501,100059808,100059726,100059315,100059401,100059901,100061001&with_stock=true&with_spu=true'
+    }).then(res => {
+      console.log(res.data.data.list)
+      this.setState({
+        recommendList: res.data.data.list
+      })
     })
   }
 }
